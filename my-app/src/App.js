0b6d69afabb70579cc12cc8192d9f242ca3525ab
Yogurt7v <egorov-2k@yandex.ru>
2023-11-22 21:30:09 +0300

    не хватает маленькой ерунды.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,9 +4,20 @@ import { Field } from './components/field/Field';
 import { Information } from './components/information/Information';
 
 export const App = () => {
-	const [currentPlayer, setCurrentPlayer] = useState('X');
-	let isGameEnded = false;
-	let isDraw = false;
+	let [currentPlayer, setCurrentPlayer] = useState('X'); // 'X' or 'O'
+	let [turnCount, setTurnCount] = useState(1);
+	let [arr, setArr] = useState(['', '', '', '', '', '', '', '', '']);
+	let [isGameEnded, setIsGameEnded] = useState(false);
+	let [isDraw, setIsDraw] = useState(false);
+
+	setCurrentPlayer(currentPlayer === 'X' ? 'O' : 'X');
+	setTurnCount(turnCount + 1);
+	setIsGameEnded = () => {
+		if (turnCount === 9) {
+			setIsDraw(true);
+			isGameEnded = true;
+		}
+	};
 
 	// if (isDraw === false && isGameEnded === true) {
 	// 	return `Победа ${currentPlayer}`;
@@ -21,8 +32,17 @@ export const App = () => {
 				<div className="App-container">
 					<header className="App-header">Крестики - Нолики</header>
 					<Information gameStatus={(isGameEnded, isDraw)}></Information>
-					<Field></Field>
-					<button className="App-restart">Restart</button>
+					<Field
+						arr={arr}
+						turnCount={turnCount}
+						symbol1={setCurrentPlayer}
+					></Field>
+					<button
+						onClick={() => setArr(['', '', '', '', '', '', '', '', ''])}
+						className="App-restart"
+					>
+						Restart
+					</button>
 				</div>
 			</div>
 		</>
--- my-app/src/components/field/Field.js
+++ my-app/src/components/field/Field.js
@@ -1,17 +1,26 @@
 import style from './field.module.css';
 
-export const Field = () => {
-	let arr = ['', '', '', '', '', '', '', '', ''];
+export const Field = (props) => {
+	let a = props.arr;
+	let s = props.symbol;
+	let t = props.turnCount;
 
-	function setSymbol(item, index) {
-		arr[index] = 'X';
-		console.log(arr);
+	function setArr(index) {
+		if (!t % 2 === 0) {
+			s = 'X';
+			t++;
+		} else {
+			s = 'O';
+			t++;
+		}
+		a[index] = `${s}`;
+		console.log(a);
 	}
 	return (
 		<div className={style.AppField}>
-			{arr.map((item, index) => (
+			{props.arr.map((item, index) => (
 				<div
-					onClick={() => setSymbol(item, index)}
+					onClick={() => setArr(index)}
 					key={index}
 					index={index}
 					className={style.AppFieldCell}
